09830ae3435e22cda3e088dc1407f837e95d36d7
kuntalroy108 <vaikartanakr@gmail.com>
2024-12-03 23:44:17 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,205 +1,191 @@
+
 // Problem__1
 
+// Create a variable called carName, assign the value Volvo to it.
 
-  function celsiusToFahrenheit(celsius) {
-    return celsius * 9/5 + 32;
-   
-  }
-  
-  console.log(celsiusToFahrenheit(0)); 
-  console.log(celsiusToFahrenheit(25)); 
- 
+let carName = "Volvo" ;
+console.log(carName);
 
 
-//   ***
+// ****
+
 
 // Problem__2
 
-
-function isEven(num) {
-    return num % 2 ===0 ;
-  }
-  
-  console.log(isEven(4)); // true
-  console.log(isEven(7)); // false
+// On one single line, declare three variables with the following names and values:
 
 
-//   ***
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+
+let firstName = "John"; 
+let lastName = "Doe" 
+let age = 35 ; 
+
+let fullInfo = `${firstName} ${lastName} ${age}`;
+console.log(fullInfo);
+
+
+//*** 
 
 // Problem___3
 
-function sum(a, b) {
-    return a + b ;
-  }
-  
-  console.log(sum(3, 4)); // 7
-  console.log(sum(10, 20)); // 30
+// Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+
+let x = 10;
+let y = 5;
+let result = x * y;
+console.log(result);
 
 
-//   ***
-  
+//***
+
+
 // Problem__4
-  
-// function findSmallestNum(arr) {
-//      return arrMath.min()
-//   }
-  
-//   console.log(findSmallestNum([3, 5, 1, 9])); // 1
-//   console.log(findSmallestNum([-1, -5, 0, 10])); // -5
+
+// Use comments to describe the correct data type of the following variables:
+
+
+let length = 16; // This is a Number data type  
+let LastName = "Johnson"; // This is a String data type ? 
+
+const X = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // This is a Object data type ? 
 
 
 
 // ***
 
-  // Problem__6
+
+// Problem___5
 
 
-  function getFirstElement(arr) {
-    return arr.at(0);
-  }
-  
-  console.log(getFirstElement([1, 2, 3])); // 1
-  console.log(getFirstElement(["a", "b", "c"])); // "a"
+// Execute the function named myFunction.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+
+// call the function and see the output 
+
+myFunction();
+
 
 
 
 // ***
 
-  // Problem___7
-  
-  function isArrayEmpty(arr) {
-    return arr.length === 0;
-  }
-  
-  console.log(isArrayEmpty([])); // true
-  console.log(isArrayEmpty([1, 2, 3])); // false
+
+// Problem___6
+
+
+// 1. Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50").
+
+
+const person = {
+    name : 'John',
+    age : 50,
+};
+
+console.log(`${person['name']} is ${person['age']}` );
 
 
 
-  // ***
-
-  // Problem___8
+// ***
 
 
-
-  // function factorialize(num) {
-  //   if (num === 0 || num === 1) return 1;
-  //   return (num-1) * num;
-  // }
-  
-  // console.log(factorialize(5)); // 120
-  // console.log(factorialize(7)); // 5040
-  
+// Problem__7
 
 
+// 1. The <button> element should do something when someone clicks on it. Try to fix it!
 
+const button = document.getElementById('btn');
 
-
-
-
-  // ***
-
-  // Problem___9
-
-
-  function reverseString(str) {
-    return str.split('').reverse().join('');
-  }
-  
-  console.log(reverseString("hello")); // "olleh"
-  console.log(reverseString("world")); // "dlrow"
-
-
-
-  
-  // ***
-
-  // Problem___10
-
-  function toLowerCase(str) {
-    return str.toLowerCase();
-  }
-  
-  console.log(toLowerCase("HELLO WORLD")); // "hello world"
-  console.log(toLowerCase("JavaScript")); // "javascript"
+button.onclick = function(){
+    alert('Thanks for clicking')
+};
 
 
 
 
-   // ***
-
-  // Problem___11
+// ***
 
 
-  function stringLength(str) {
-    return str.length;
-  }
-  
-  console.log(stringLength("hello")); // 5
-  console.log(stringLength("world")); // 5
+// Problem__8
 
 
+// 1. Alert the number of items in an array, using the correct Array property: 
 
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
 
-  // ***
+// 2. Change the first item of Brand to "Ford".
 
-  // Problem___12
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
 
+    Brand[0] = 'Ford';
 
-  function mergeArrays(arr1, arr2) {
-    return arr1.concat(arr2);;
-  }
-  
-  console.log(mergeArrays([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
-  console.log(mergeArrays(["a", "b"], ["c", "d"])); // ["a", "b", "c", "d"]
-  
-  
+    console.log(Brand);
 
 
 
-  // ***
 
-  // Problem___13
-
-
-  function getLastElement(arr) {
-    return arr.pop();
-  }
-  
-  console.log(getLastElement([1, 2, 3])); // 3
-  console.log(getLastElement(["a", "b", "c"])); // "c"
-  
-  
-  
-
-  // ***
-
-  // Problem___14
+    
+// ***
 
 
-  function getFirstCharacter(str) {
-    return str.charAt();
-  }
-  
-  console.log(getFirstCharacter("hello")); // "h"
-  console.log(getFirstCharacter("world")); // "w"
-  
-  
-
-  
-  // ***
-
-  // Problem___15
+// Problem__9
 
 
-  function sumArray(arr) {
-      return arr.reduce((accumulator,current) => {
-        return accumulator + current;
-      })
-    };
-  
-  
-  console.log(sumArray([1, 2, 3, 4])); // 10
-  console.log(sumArray([-1, -2, -3, -4])); // -10
-  console.log(sumArray([1.5, 2.5, 3.5])); // 7.5
-  
+// 1. Use the correct Math method to create a random number.
+
+console.log(Math.random());
+
+
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+
+let a = Math.max(10, 20);
+console.log(a);
+
+// 3. Use the correct Math method to get the square root of 9.
+
+const Sqroot = Math.sqrt(9);
+console.log(Sqroot);
+
+
+
+
+
+// ***
+
+// Problem__10
+
+
+// 1. Choose the correct comparison operator to alert true, when x is greater than y.
+
+x1 = 10;
+y1 = 5;
+
+console.log(x1 > y1); 
+
+
+
+// 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+
+
+let Age = 16;
+let message;
+message = Age <= 18 ? 'too young' : 'Old enough'; 
+console.log(message);
+
+
+
+
+
+
